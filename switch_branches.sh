912f35642e23812cc98520744ca7a219580ed444
Brandon Waskiewicz <brandon@pagerduty.com>
2024-01-18 14:49:46 -0500

    Improve sb script

--- switch_branches.sh
+++ switch_branches.sh
@@ -4,12 +4,26 @@
 i=1
 declare -a branches
 
-# list local branches in plain format and loop through them
+# prioritize main/master first
 while read -r branchname ; do
   branches[$i]="$branchname"
   printf "%2s    %s\n" $i $branchname
   ((i++))
-done < <(git for-each-ref --format='%(refname:short)' refs/heads/)
+done < <(git for-each-ref --format='%(refname:short)' refs/heads/ | rg '^(main|master)$')
+
+# then list non-deploy branches
+while read -r branchname ; do
+  branches[$i]="$branchname"
+  printf "%2s    %s\n" $i $branchname
+  ((i++))
+done < <(git for-each-ref --format='%(refname:short)' refs/heads/ | rg -v '^(main|master|deploy/.+)$')
+
+# then list deploy branches
+while read -r branchname ; do
+  branches[$i]="$branchname"
+  printf "%2s    %s\n" $i $branchname
+  ((i++))
+done < <(git for-each-ref --format='%(refname:short)' refs/heads/ | rg '^(deploy/.+)$')
 
 printf "switch to: "
 # read the user's selected integer
